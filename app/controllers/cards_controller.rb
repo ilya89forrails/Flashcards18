afab26715d7aa364bf4f764af7f901f23f76dd57
ilya <ilya89forrails@gmail.com>
2018-07-25 22:04:53 +0900

    les3_1

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,7 @@
 class CardsController < ApplicationController
+
+  before_action :set_card, only: [:edit, :show, :update, :destroy]
+
   def index
     @cards = Card.all
   end
@@ -8,28 +11,26 @@ class CardsController < ApplicationController
   end
 
   def edit
-    @card = Card.find(params[:id])
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   def create
     @card = Card.new(card_params)
     @card.review_date = Date.today + 3.days
-    @card.save
-    redirect_to cards_path
+    if @card.save
+      redirect_to cards_path
+    else
+      render 'new'
   end
 
   def update
-    @card = Card.find(params[:id])
     @card.update(card_params)
     redirect_to cards_path
   end
 
   def destroy
-    @card = Card.find(params[:id])
     @card.destroy
     redirect_to cards_path
   end
@@ -39,4 +40,9 @@ class CardsController < ApplicationController
   def card_params
     params.require(:card).permit(:original_text, :translated_text, :review_date)
   end
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
 end
